cc65062fa0594642e6e44a4dc3fceb01e6a9cdb1
Boy <sivertpaulsen2@gmail.com>
2021-09-11 22:01:26 +0200

    add cancellation option

--- geary-commons-papermc/src/main/kotlin/com/mineinabyss/geary/minecraft/actions/PlaceBlockAction.kt
+++ geary-commons-papermc/src/main/kotlin/com/mineinabyss/geary/minecraft/actions/PlaceBlockAction.kt
@@ -12,18 +12,21 @@ import org.bukkit.Material
  *
  * Summons a block at given location.
  *
+ * @param cancel If block placing should be cancelled.
  * @param at The location to spawn block at.
  * @param blockType The block to spawn.
  */
 @Serializable
 @SerialName("geary:place_block")
-public data class PlaceBlockAction(
+data class PlaceBlockAction(
+    val cancel: Boolean = false,
     val at: ConfigurableLocation,
     val blockType: Material,
 ) : GearyAction() {
     private val GearyEntity.location by at
 
     override fun GearyEntity.run(): Boolean {
+        if (!cancel)
         location.block.type = blockType
         return true
     }
