f2822ea8614a2be3111eb1ca55c0ef7e60a36066
Boy <sivertpaulsen2@gmail.com>
2021-10-18 12:44:37 +0200

    initial soulbind commit

---
+++ geary-commons-papermc/src/main/kotlin/com/mineinabyss/geary/minecraft/components/Soulbound.kt
@@ -0,0 +1,14 @@
+package com.mineinabyss.geary.minecraft.components
+
+import com.mineinabyss.geary.ecs.api.autoscan.AutoscanComponent
+import com.mineinabyss.idofront.serialization.UUIDSerializer
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import java.util.*
+
+@Serializable
+@SerialName("geary:soulbound")
+@AutoscanComponent
+class Soulbound(
+    var owner: @Serializable(with = UUIDSerializer::class)UUID
+)
