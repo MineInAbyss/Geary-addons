476637df7c889eecb3761acff2a6c1aadc24ad3a
Boy <sivertpaulsen2@gmail.com>
2021-10-25 03:10:22 +0200

    fix oopsies

--- geary-commons-papermc/src/main/kotlin/com/mineinabyss/geary/minecraft/systems/ItemStackingListener.kt
+++ geary-commons-papermc/src/main/kotlin/com/mineinabyss/geary/minecraft/systems/ItemStackingListener.kt
@@ -2,70 +2,98 @@ package com.mineinabyss.geary.minecraft.systems
 
 import com.mineinabyss.geary.ecs.entities.prefabs
 import com.mineinabyss.geary.minecraft.components.Stackable
-import com.mineinabyss.idofront.messaging.broadcast
+import com.mineinabyss.idofront.messaging.broadcastVal
 import com.mineinabyss.looty.tracking.toGearyOrNull
 import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.inventory.InventoryAction
 import org.bukkit.event.inventory.InventoryClickEvent
 
 object ItemStackingListener : Listener {
 
     @EventHandler
     fun InventoryClickEvent.onCombineItems() {
+        action.broadcastVal("Action: ")
         val player = inventory.holder as? Player ?: return
+        "inv click".broadcastVal()
         val currItem = currentItem
-        val cursor = cursor ?: return
-        val gearyCursor = cursor.toGearyOrNull(player) ?: return
+        val cursor = cursor!!
+        val gearyCursor = cursor.toGearyOrNull(player)
+        "after cursor".broadcastVal()
         val gearyCurrentItem = currItem?.toGearyOrNull(player)
         val stackable = gearyCurrentItem?.get<Stackable>()
 
-        isCancelled = true//
+        if (gearyCurrentItem?.has<Stackable>() != true && gearyCursor?.has<Stackable>() != true) return
 
-        if (gearyCurrentItem?.prefabs != gearyCursor.prefabs && currentItem!!.type != Material.AIR) return
+        isCancelled = true
 
-        if (isLeftClick) {
-            if (currentItem?.type == Material.AIR) {
-                currentItem = cursor.clone()
-                view.cursor = null
-                return
-            }
-            while (currItem!!.amount < stackable!!.stackSize && view.cursor?.amount!! > 0) {
-                currItem.amount += 1
-                view.cursor?.subtract()
-            }
-            isCancelled = true
-            return
-        }
+        if (gearyCurrentItem?.prefabs != gearyCursor?.prefabs && currentItem?.type != Material.AIR && cursor.type != Material.AIR) return
 
-        if (isRightClick) {
-            if (currentItem?.type == Material.AIR) {
-                currentItem = cursor.clone()
-                currentItem?.amount = 1
-                view.cursor?.subtract()
-            }
-            if (currItem!!.amount < stackable!!.stackSize) {
-                currItem.amount += 1
-                view.cursor?.subtract()
-            }
-            isCancelled = true
-            return
-        }
-
-        if (isShiftClick) {
-            broadcast("bruh")
-            inventory.contents.forEach {
-                val item = it.toGearyOrNull(player)
-                item?.get<Stackable>() ?: return
-                if (currentItem!!.amount + it.amount <= stackable!!.stackSize) {
-                    it.amount += currentItem!!.amount
-                    currentItem?.amount = 0
+        when {
+            action == InventoryAction.COLLECT_TO_CURSOR -> {
+                player.inventory.contents.forEach {
+                    if (it == null) return@forEach
+                    val gearyIt = it.toGearyOrNull(player) ?: return@forEach
+                    if (gearyIt.prefabs != gearyCursor?.prefabs) return@forEach
+                    while (cursor.amount < gearyCursor.get<Stackable>()?.stackSize!! && it.amount > 0) {
+                        cursor.amount += it.amount
+                        it.subtract(it.amount)
+                        return@forEach
+                    }
+                    return@forEach
                 }
-                return@forEach
             }
-            isCancelled = true
-            return
+            isLeftClick && !isShiftClick -> {
+                if (cursor.type == Material.AIR) {
+                    view.cursor = currentItem?.clone()
+                    currentItem = null
+                    return
+                }
+                if (currentItem?.type == Material.AIR) {
+                    currentItem = cursor.clone()
+                    view.cursor = null
+                    return
+                }
+                while (currItem!!.amount < stackable!!.stackSize && view.cursor?.amount!! > 0) {
+                    currItem.amount += 1
+                    view.cursor?.subtract()
+                }
+            }
+            isRightClick && !isShiftClick -> {
+                if (cursor.type == Material.AIR) {
+                    val clone = currentItem?.clone()
+                    currentItem?.apply { amount /= 2 } // Takes the bigger half on odd-numbers like vanilla
+                    view.cursor = clone?.apply { amount -= currentItem?.amount!! }
+                    return
+                }
+                if (currentItem?.type == Material.AIR) {
+                    currentItem = cursor.clone()
+                    currentItem?.amount = 1
+                    view.cursor!!.subtract()
+                    return
+                }
+                if (currItem!!.amount < stackable!!.stackSize) {
+                    currItem.amount += 1
+                    view.cursor?.subtract()
+                }
+            }
+            //TODO too hard get fucked its 3am i wanna sleep
+//            isShiftClick -> {
+//                broadcast("bruh")
+//                inventory.contents.forEach {
+//                    val item = it.toGearyOrNull(player)
+//                    item?.get<Stackable>() ?: return@forEach
+//                    if (currentItem!!.amount + it.amount <= stackable!!.stackSize) {
+//                        it.amount += currentItem!!.amount
+//                        currentItem?.amount = 0
+//                    }
+//                    return@forEach
+//                }
+//                return
+//            }
+            else -> isCancelled = false
         }
     }
 }
