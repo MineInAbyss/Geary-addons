de84e8e3982660983826f7826d455e33e3ddc70a
Boy <sivertpaulsen2@gmail.com>
2022-08-16 17:19:08 +0200

    use held item not event-item due to geary not recognizing it

--- geary-commons-papermc/src/main/kotlin/com/mineinabyss/geary/papermc/systems/CustomFoodSystem.kt
+++ geary-commons-papermc/src/main/kotlin/com/mineinabyss/geary/papermc/systems/CustomFoodSystem.kt
@@ -6,26 +6,35 @@ import org.bukkit.GameMode
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerItemConsumeEvent
-import org.bukkit.potion.PotionEffect
+import org.bukkit.inventory.EquipmentSlot
 import kotlin.random.Random
 
 class CustomFoodSystem : Listener {
 
     @EventHandler
     fun PlayerItemConsumeEvent.onConsumeFood() {
-        val gearyFood = item.toGearyOrNull(player)?.get<Food>() ?: return
-        isCancelled = true // Cancel vanilla behaviour
-        if (player.gameMode == GameMode.CREATIVE) return
-
-        if (gearyFood.replacement != null) setItem(gearyFood.replacement.toItemStack()) else item.subtract()
-        if (gearyFood.effectList.isNotEmpty() && gearyFood.effectChance > 0.0) {
-            if (Random.nextDouble(0.0, 1.0) <= gearyFood.effectChance)
-                gearyFood.effectList.forEach { e ->
-                    player.addPotionEffect(PotionEffect(e.type, e.duration, e.amplifier, e.isAmbient, e.hasParticles(), e.hasIcon()))
-                }
+        val inv = player.inventory
+        val hand = when {
+            inv.itemInMainHand.isSimilar(item) -> EquipmentSlot.HAND
+            inv.itemInOffHand.isSimilar(item) -> EquipmentSlot.OFF_HAND
+            else -> return
         }
+        val item = if (hand == EquipmentSlot.HAND) inv.itemInMainHand else inv.itemInOffHand
+        val gearyFood = item.toGearyOrNull(player)?.get<Food>() ?: return
 
-        player.foodLevel += minOf(gearyFood.hunger, 20)
-        player.saturation += minOf(gearyFood.saturation, 20)
+        isCancelled = true // Cancel vanilla behaviour
+
+        if (player.gameMode != GameMode.CREATIVE) {
+            if (gearyFood.replacement != null)
+                if (hand == EquipmentSlot.HAND) inv.setItemInMainHand(gearyFood.replacement.toItemStack())
+                else inv.setItemInOffHand(gearyFood.replacement.toItemStack())
+            else item.subtract()
+
+            if (gearyFood.effectList.isNotEmpty() && Random.nextDouble(0.0, 1.0) <= gearyFood.effectChance)
+                gearyFood.effectList.forEach { effect -> player.addPotionEffect(effect) }
+
+            player.foodLevel += minOf(gearyFood.hunger, 20)
+            player.saturation += minOf(gearyFood.saturation, 20)
+        }
     }
 }
